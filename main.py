53829315d72a9a999530f8661683d4ade7e2d382
chase-west <chasetwest@gmail.com>
2023-11-20 19:15:25 -0800

    Made functional

--- main.py
+++ main.py
@@ -4,61 +4,55 @@ import random
 import speech_recognition as sr
 import os 
 from difflib import SequenceMatcher
-import time
 
 spanOrEnglish = input("Would you like to translate to spanish or english? (s/e): ")
-amountOfWords = input("How many words would you like to learn: ")
 
 if spanOrEnglish == "s":
+   print("Spanish mode selected.")
    mode = "spanish"
 
 else :
+   print("English mode selected.")
    mode = "english"
 
+amountOfWords = input("\nHow many words would you like to learn (a for all): ")
 
 #Create dict of vocab words
 vocab = {
-  "hello": "hola",
-  "goodbye": "adiós",
-  "good morning": "buenos días",
-  "good afternoon": "buenas tardes",
-  "good evening": "buenas noches",
-  "how are you?": "¿cómo estás?",
-  "good": "bien",
-  "bad": "mal",
-  "thank you": "gracias",
-  "please": "por favor",
-  "you're welcome": "de nada",
-  "excuse me": "con permiso",
-  "I'm sorry": "lo siento",
-  "yes": "sí",
-  "no": "no",
-  "I don't understand": "no entiendo",
-  "I don't know": "no sé",
-  "what's your name?": "¿cómo te llamas?",
-  "my name is": "me llamo",
-  "where are you from?": "¿de dónde eres?",
-  "I'm from": "soy de",
-  "how old are you?": "¿cuántos años tienes?",
-  "I'm": "tengo",
-  "what time is it?": "¿qué hora es?",
-  "it's": "son las",
-  "what day is it?": "¿qué día es?",
-  "today is": "hoy es",
-  "tomorrow is": "mañana es",
-  "yesterday was": "ayer fue",
-  "what is this?": "¿qué es esto?",
-  "this is": "esto es",
-  "how much does it cost?": "¿cuánto cuesta?",
-  "where is the bathroom?": "¿dónde está el baño?",
-  "where is the bus stop?": "¿dónde está la parada de autobús?",
-  "where is the train station?": "¿dónde está la estación de tren?",
-  "where is the airport?": "¿dónde está el aeropuerto?",
-  "where is the hotel?": "¿dónde está el hotel?",
-  "where is the restaurant?": "¿dónde está el restaurante?",
-  "where is the bank?": "¿dónde está el banco?",
-  "where is the pharmacy?": "¿dónde está la farmacia?",
+  "peinarse": "to comb",
+  "este, esta (estos, estas)": "this (one) (these [ones])",
+  "esto": "this (neuter form)",
+  "quitarse": "to take off",
+  "ducharse": "to shower",
+  "desayunar": "to have breakfast",
+  "el peine": "comb",
+  "así": "thus, that way",
+  "la tina": "bathtub",
+  "la ducha": "shower",
+  "despertarse (ie)": "to wake up",
+  "lavarse": "to wash",
+  "quemarse": "to get burned",
+  "la toalla": "towel",
+  "calmarse": "to calm down",
+  "eso": "that (neuter form)",
+  "la cena": "dinner, supper",
+  "ese, esa (esos, esas)": "that (one) (those [ones])",
+  "el jabón": "soap",
+  "tarde": "late",
+  "acostarse (ue)": "to go to bed, to lie down",
+  "la comida": "midday meal",
+  "el pelo": "hair",
+  "el cepillo": "brush",
+  "preocuparse": "to worry",
+  "afeitarse": "to shave",
+  "almorzar (ue)": "to have lunch, to eat lunch",
+  "el lavabo": "sink",
+  "desde luego": "of course",
+  "el inodoro": "toilet"
 }
+
+if amountOfWords == "a":
+    amountOfWords = len(vocab)
 
 #Function to check if two strings are similar
 def similarity(a, b):
@@ -66,79 +60,64 @@ def similarity(a, b):
 
 
 completedWords = []
+wordsDone = 0
 
-while len(completedWords) <= int(amountOfWords):
-  
+for spanishWord, englishWord in vocab.items():
+    if englishWord not in completedWords and spanishWord not in completedWords and wordsDone < int(amountOfWords):
+        if mode == "english":
+            # Text to speech for Spanish
+            tts = gTTS(text=spanishWord, lang="es")
+            tts.save("current.mp3")
+            playsound("current.mp3")
+            os.remove("current.mp3")
 
-  #Choose random word from dict
-  for i in range(len(vocab)):
-      englishWord, spanishWord = random.choice(list(vocab.items()))
+            # Recognize speech for English
+            r = sr.Recognizer()
+            text = "default"
 
-      if englishWord not in completedWords and spanishWord not in completedWords:
-          completedWords.append(englishWord)
-          completedWords.append(spanishWord)
-          break
+            with sr.Microphone() as source:
+                try:
+                    audio = r.listen(source)
+                    text = r.recognize_google(audio)
+                except:
+                    pass
 
-  if mode == "english":
-    #Text to speech for spanish 
-    tts = gTTS(text=spanishWord, lang="es")
-    tts.save("current.mp3")
-    time.sleep(2)
-    playsound("current.mp3")
-    os.remove("current.mp3")
+            similar = similarity(text, englishWord)
+            print(similar)
+            if similar >= 0.5:
+                print("Correct!")
+            elif text == "default":
+                print("No input detected. The correct answer is " + englishWord + ".")
+            else:
+                print("Incorrect. The correct answer is " + englishWord + ". You said " + text + ".")
 
-    #Recognize speech for english 
-    r = sr.Recognizer()
-    text = "default"
+        elif mode == "spanish":
+            tts = gTTS(text=englishWord, lang="en")
+            tts.save("current.mp3")
+            playsound("current.mp3")
+            os.remove("current.mp3")
 
-    with sr.Microphone() as source:
-        try:
-            audio = r.listen(source)
-            text = r.recognize_google(audio)
-        except:
-            pass
-      
+            # Recognize speech for English
+            r = sr.Recognizer()
+            text = "default"
 
-    similar = similarity(text, englishWord)
-    print(similar)
-    if similar >= 0.5:
-      print("Correct!")
+            with sr.Microphone() as source:
+                try:
+                    audio = r.listen(source)
+                    text = r.recognize_google(audio, language="es")
+                except:
+                    pass
 
-    elif text == "default":
-      print("No input detected. The correct answer is " + englishWord + ".")
+            similar = similarity(text, spanishWord)
+            print(similar)
+            if similar >= 0.5:
+                print("Correct!")
+            elif text == "default":
+                print("No input detected. The correct answer is " + spanishWord + ".")
+            else:
+                print("Incorrect. The correct answer is " + spanishWord + ". You said " + text + ".")
 
-    else: 
-      print("Incorrect. The correct answer is " + englishWord + "." + " You said " + text + ".")
-    completedWords.append(englishWord)
-    completedWords.append(spanishWord)
-
-  elif mode == "spanish":
-    tts = gTTS(text=englishWord, lang="en")
-    tts.save("current.mp3")
-    playsound("current.mp3")
-    os.remove("current.mp3")
-
-    #Recognize speech for english 
-    r = sr.Recognizer()
-    text = "default"
-
-    with sr.Microphone() as source:
-        try:
-            audio = r.listen(source)
-            text = r.recognize_google(audio, language="es")
-        except:
-            pass
-
-    similar = similarity(text, spanishWord)
-    print(similar)
-    if similar >= 0.5:
-      print("Correct!")
-
-    elif text == "default":
-      print("No input detected. The correct answer is " + spanishWord + ".")
-
-    else: 
-      print("Incorrect. The correct answer is " + spanishWord + "." + " You said " + text + ".")
-    completedWords.append(englishWord)
-    completedWords.append(spanishWord)
+        completedWords.append(englishWord)
+        completedWords.append(spanishWord)
+        wordsDone += 1
     
