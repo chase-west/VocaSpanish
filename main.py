525f247c4ea183390012fae92f83f8b1fd408e08
chase-west <chasetwest@gmail.com>
2023-11-25 18:51:06 -0800

    Simplified code with function

--- main.py
+++ main.py
@@ -5,15 +5,23 @@ import speech_recognition as sr
 import os 
 from difflib import SequenceMatcher
 
-spanOrEnglish = input("Would you like to translate to spanish or english? (s/e): ")
+choice = "invalid"
+while choice == "invalid":
+    spanOrEnglish = input("Would you like to translate to spanish or english? (s/e): ")
 
-if spanOrEnglish == "s":
-   print("Spanish mode selected.")
-   mode = "spanish"
+    if spanOrEnglish == "s":
+        print("Spanish mode selected.")
+        mode = "spanish"
+        choice = "valid"
 
-else :
-   print("English mode selected.")
-   mode = "english"
+    elif spanOrEnglish == "e" :
+        print("English mode selected.")
+        mode = "english"
+        choice = "valid"
+
+    else:
+        print("Invalid input.")
+        choice = "invalid"
 
 amountOfWords = input("\nHow many words would you like to learn (a for all): ")
 
@@ -133,10 +141,15 @@ vocab = {
 if amountOfWords == "a":
     amountOfWords = len(vocab)
 
-#Function to check if two strings are similar
-def similarity(a, b):
-  return (SequenceMatcher(None, a, b).ratio())
-
+#Function to check similarity of words
+def check_similarity(lanugageWord, text):
+  similarity = (SequenceMatcher(None, lanugageWord, text).ratio())
+  if similarity >= 0.46:
+    print("Correct!")
+  elif text == "default":
+    print("No input detected. The correct answer is " + lanugageWord + ".")
+  else:
+    print("Incorrect. The correct answer is " + lanugageWord + ". You said " + text + ".")
 
 completedWords = []
 wordsDone = 0
@@ -161,14 +174,7 @@ for spanishWord, englishWord in vocab.items():
                 except:
                     pass
 
-            similar = similarity(text, englishWord)
-            print(similar)
-            if similar >= 0.5:
-                print("Correct!")
-            elif text == "default":
-                print("No input detected. The correct answer is " + englishWord + ".")
-            else:
-                print("Incorrect. The correct answer is " + englishWord + ". You said " + text + ".")
+            check_similarity(englishWord, text)
 
         elif mode == "spanish":
             tts = gTTS(text=englishWord, lang="en")
@@ -187,16 +193,8 @@ for spanishWord, englishWord in vocab.items():
                 except:
                     pass
 
-            similar = similarity(text, spanishWord)
-            print(similar)
-            if similar >= 0.5:
-                print("Correct!")
-            elif text == "default":
-                print("No input detected. The correct answer is " + spanishWord + ".")
-            else:
-                print("Incorrect. The correct answer is " + spanishWord + ". You said " + text + ".")
+            check_similarity(spanishWord, text)
 
         completedWords.append(englishWord)
         completedWords.append(spanishWord)
-        wordsDone += 1
-    
+        wordsDone += 1    
